57731ccdd08eb57ffdcf1617aff715df0a31ff2e
HeoJooYoung <jyhur900226@naver.com>
2017-10-30 20:45:40 +0900

    [Work] download image without callback

--- downloadImage.js
+++ downloadImage.js
@@ -4,10 +4,16 @@ const request = require('request');
 let imageUrl = 'https://cdn.pixabay.com/photo/2017/10/27/20/25/leopard-2895448_960_720.jpg';
 let filename = 'leopard.jpg';
 
-downloadImage(imageUrl, filename, () => {
-  console.log('download image done');
-});
+// downloadImage(imageUrl, filename, () => {
+//   console.log('download image done');
+// });
+//
+// function downloadImage(uri, filename, callback) {
+//   request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+// }
 
-function downloadImage(uri, filename, callback) {
-  request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+downloadImage(imageUrl, filename);
+
+function downloadImage(uri, filename) {
+  request(uri).pipe(fs.createWriteStream(filename));
 }
