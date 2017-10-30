14d69e37d51f7ad3c51d4e28154b8c8d36b5d806
HeoJooYoung <jyhur900226@naver.com>
2017-10-30 20:54:19 +0900

    [Work] download multiple images

--- downloadImage.js
+++ downloadImage.js
@@ -1,8 +1,31 @@
 const fs = require('fs');
 const request = require('request');
 
-let imageUrl = 'https://cdn.pixabay.com/photo/2017/10/27/20/25/leopard-2895448_960_720.jpg';
-let filename = 'leopard.jpg';
+let imageUrlList = [
+  'https://cdn.pixabay.com/photo/2017/10/27/20/25/leopard-2895448_960_720.jpg',
+  'https://cdn.pixabay.com/photo/2017/10/26/20/00/pumpkin-2892303_960_720.jpg',
+  'https://cdn.pixabay.com/photo/2017/10/26/23/58/rose-2892821_960_720.jpg',
+  'https://cdn.pixabay.com/photo/2017/10/20/21/45/adler-2872995_960_720.jpg',
+  'https://cdn.pixabay.com/photo/2017/10/25/16/54/african-lion-2888519_960_720.jpg'
+];
+
+for (let i in imageUrlList) {
+  let item = imageUrlList[i];
+  let filename = `${convertUrlToFilename(item)}.jpg`;
+
+  downloadImage(item, filename);
+}
+
+function convertUrlToFilename(url) {
+  let splitList = url.split('/');
+  let last = splitList[splitList.length - 1];
+
+  return last.split('-')[0];
+}
+
+function downloadImage(uri, filename) {
+  request(uri).pipe(fs.createWriteStream(filename));
+}
 
 // downloadImage(imageUrl, filename, () => {
 //   console.log('download image done');
@@ -11,9 +34,3 @@ let filename = 'leopard.jpg';
 // function downloadImage(uri, filename, callback) {
 //   request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
 // }
-
-downloadImage(imageUrl, filename);
-
-function downloadImage(uri, filename) {
-  request(uri).pipe(fs.createWriteStream(filename));
-}
